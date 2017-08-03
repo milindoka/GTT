343d4e28241d918e4d85f6f8f2f1408a49229f6f
milindoka <oak444@gmail.com>
2017-08-03 09:45:18 +0530

    Added Grid To Frame

--- GTT.cpp
+++ GTT.cpp
@@ -3,5 +3,13 @@
 GTT::GTT(const wxString& title)
        : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(250, 150))
 {
+      WxGrid1 = new wxGrid(this, 1005, wxPoint(16, 83), wxSize(480, 289), wxRAISED_BORDER);
+	WxGrid1->SetFont(wxFont(8, wxSWISS, wxNORMAL, wxNORMAL, false, wxT("Segoe UI")));
+	WxGrid1->SetDefaultColSize(80);
+	WxGrid1->SetDefaultRowSize(20);
+	WxGrid1->SetRowLabelSize(20);
+	WxGrid1->SetColLabelSize(25);
+	WxGrid1->CreateGrid(12,8,wxGrid::wxGridSelectRows);
+  
   Centre();
 }
--- GTT.h
+++ GTT.h
@@ -1,8 +1,10 @@
 #include <wx/wx.h>
+#include <wx/grid.h>
 
 class GTT : public wxFrame
 {
 public:
     GTT(const wxString& title);
+    wxGrid *WxGrid1;
 
 };
